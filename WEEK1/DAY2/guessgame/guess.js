452522727a57706f0ba453512e556b17a086e626
oshowunm <oluwaseunshowunmi97@gmail.com>
2026-09-22 12:33:53 +0100

    guessgame

--- WEEK1/DAY2/guessgame/guess.js
+++ WEEK1/DAY2/guessgame/guess.js
@@ -1,26 +1,51 @@
-// alert('Welcome')
+
 const secretNumber = Math.floor(Math.random() * 100) + 1
+
 const keepGuess = []
 
 function guessNumber (number) {
+    alert(`Welcome to my Guess-Game, Player 🤗` )
+
     let guess = 0;
 
     while (guess < 7) {
 
-    const number = Number(window.prompt("Guess the secret number"))
+    let number = Number(window.prompt("Guess the secret number"))
     keepGuess.push(number)
     guess += 1
 
     if (number === secretNumber) {
         alert(`Congratulations, you got it in ${guess} attempt!!!`)
-    } else if (number > secretNumber) {
-        alert("Number too high, try again!")
-    } else if (number < secretNumber) {
-        alert("Number too low, try again!")
-    } else {
-        break
-    }
+        console.log(`Congratulations, you got it in ${guess} attempt!!!`)
+        break;
     } 
 
-    console.log(`Your nummber of guesses: ${keepGuess}`)
+    if (guess === 7) {
+        alert(`Sorry, you are out of attempt for now!!!`)
+        console.log("Sorry, you are out of attempt for now!!!")
+        break;
+    }
+    
+    if (number > secretNumber) {
+        alert("Number too high, try again!")
+        console.log("Number too high, try again!")
+        // break;
+    } 
+    
+    if (number < secretNumber) {
+        alert("Number too low, try again!")
+        console.log("Number too low, try again!")
+    }
+
+    if (isNaN(number)) {
+        alert("Invalid Number, kindly enter digits only!!!")
+        console.log("Invalid Number, kindly enter digits only!!!")
+    }
+
+
+    } 
+
+    console.log(`Your guesses: (${guess}), ${keepGuess}`)
 }
+
+guessNumber()
