28f370c213fbd78d028b21e165b1734a4bd0f187
oshowunm <seunfunmiajoke@gmail.com>
2026-09-25 23:31:15 +0100

    refactored function divide

--- WEEK1/DAY3/calculator.js
+++ WEEK1/DAY3/calculator.js
@@ -1,7 +1,13 @@
 function add(a,b) {return a + b}
 const subtract = function(a,b) {return a - b}
 const multiply = (a, b) => (a * b)
-const divide = (a, b) => (a / b)
+
+function divide (a, b) {
+    if (b === 0) {
+        return "Error: Division by zero is not allowed."
+    }
+    return a / b
+}
 
 console.log(add(10, 4))
 console.log(subtract(10, 4))
@@ -11,9 +17,9 @@ console.log(divide(10, 4))
 function calculate(a, b) {
     return function add() {
         return a+b
-    }
-    
+ }    
 }
+// Is it possible to return two functions in one function
 
 const result = calculate(2,4)
 console.log(result())
